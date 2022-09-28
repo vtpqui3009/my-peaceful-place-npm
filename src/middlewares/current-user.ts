4c28b574b81ae176253b093eab13ababadc2bde0
vtpqui <vtpqui@gmail.com>
2022-09-28 20:31:17 +0700

    first commit

---
+++ src/middlewares/current-user.ts
@@ -0,0 +1,41 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+interface UserPayload {
+  id: string;
+  email: string;
+  name: string;
+  isAdmin: boolean;
+  defaultProvidedStorageCapacity: string;
+  remainningStorageCapacity: string;
+  isDeleted: boolean;
+}
+
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: UserPayload;
+    }
+  }
+}
+export const currentUser = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (!req.session?.jwt) {
+    return next();
+  }
+
+  try {
+    const payload = jwt.verify(
+      req.session.jwt,
+      process.env.JWT_KEY!
+    ) as UserPayload;
+    req.currentUser = payload;
+  } catch (error) {
+    next(error);
+  }
+  next();
+};
