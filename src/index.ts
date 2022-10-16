802832fb8cd12951974ab8075cf0bf6b44967f07
vtpqui <vtpqui@gmail.com>
2022-10-16 15:37:35 +0700

    Updates

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,13 @@ export * from "./errors/database-connection-error";
 export * from "./errors/not-authorized-error";
 export * from "./errors/not-found-error";
 export * from "./errors/request-validation-error";
+export * from "./errors/not-have-permission";
 
 export * from "./middlewares/current-user";
 export * from "./middlewares/error-handler";
 export * from "./middlewares/require-auth";
 export * from "./middlewares/validate-request";
+export * from "./middlewares/authorize-role";
 
 export * from "./cloudinary/uploadAvatar";
 export * from "./utils/multer";
--- src/middlewares/authorize-role.ts
+++ src/middlewares/authorize-role.ts
@@ -1,45 +1,14 @@
 import { Request, Response, NextFunction } from "express";
-import jwt from "jsonwebtoken";
 import { NotHavePermissionError } from "../errors/not-have-permission";
-interface UserPayload {
-  id: string;
-  email: string;
-  username: string;
-  isAdmin: boolean;
-  defaultProvidedStorageCapacity: string;
-  remainningStorageCapacity: string;
-  isDeleted: boolean;
-}
 
-declare global {
-  namespace Express {
-    interface Request {
-      currentUser?: UserPayload;
-    }
-  }
-}
-export const authorizeAdmin = (
+export const authorizedAdmin = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  if (!req.session?.jwt) {
-    return next();
+  if (!req.currentUser?.isAdmin) {
+    throw new NotHavePermissionError("Just admin is allowed to do this action");
   }
-
-  try {
-    const payload = jwt.verify(
-      req.session.jwt,
-      "vievie1009"
-      // process.env.JWT_KEY!
-    ) as UserPayload;
-    if (req.currentUser?.isAdmin) {
-      next();
-    } else {
-      throw new NotHavePermissionError("Just admin can do this action");
-    }
-    req.currentUser = payload;
-  } catch (err) {}
 
   next();
 };
